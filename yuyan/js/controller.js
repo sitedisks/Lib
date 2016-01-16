c57f0abb549e58cb025c4bd71028edb941df8731
Peter <peter@sitedisks.com.au>
2016-01-16 13:11:48 +1100

    markers

--- yuyan/js/controller.js
+++ yuyan/js/controller.js
@@ -16,8 +16,20 @@
 				  
 				$scope.map = { 
 					center: { latitude: 45, longitude: -73 }, 
-					zoom: 8 
+					zoom: 8,
+					options: { scrollwheel: true},
+					control: {}
 				};
+				
+				//single marker
+				$scope.marker = {id: 0, coords: { latitude: 45, longitude: -73 }};
+				
+				//multiple markers
+				$scope.markers = [
+					{id: 0, coords: { latitude: 45, longitude: -73 }, info: "marker1"},
+					{id: 1, coords: { latitude: 46, longitude: -72 }, info: "marker2"},
+					{id: 2, coords: { latitude: 47, longitude: -71 }, info: "marker3"}
+				];
 				
 			})
 		

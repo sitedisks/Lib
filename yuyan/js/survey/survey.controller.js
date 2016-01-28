d178438809d18a3ec82a619e907b13162c1586aa
Peter <peter@sitedisks.com.au>
2016-01-28 22:20:30 +1100

    YuYan -  Chorice

---
+++ yuyan/js/survey/survey.controller.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+  angular.module('yuyanApp').controller('surveyCtrl',
+  ['$scope', function($scope){
+
+    $scope.message = 'Now you have a chorice!';
+
+  }]);
+
+})();

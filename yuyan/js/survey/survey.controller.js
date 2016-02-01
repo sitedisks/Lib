f194fc2dee5849bbf76264bb6879672944a68dd3
Peter Wang <peter@sitedisks.com.au>
2016-02-01 18:06:23 +1100

    temp work

--- yuyan/js/survey/survey.controller.js
+++ yuyan/js/survey/survey.controller.js
@@ -2,8 +2,41 @@
   'use strict';
   angular.module('yuyanApp').controller('surveyCtrl',
   ['$scope', function($scope){
-
-    $scope.message = 'Now you have a chorice!';
+	var surveyModel = {
+	  "Title": "string",
+	  "ShortDesc": "string",
+	  "LongDesc": "string",
+	  "UserId": "string",
+	  "dtoQuestions": [
+		{
+		  "QuestionId": 0,
+		  "SurveyId": 0,
+		  "Question": "string",
+		  "QuestionOrder": 0,
+		  "QuestionType": 1,
+		  "dtoItems": [
+			{
+			  "QuestionItemId": 0,
+			  "QuestionId": 0,
+			  "ItemDescription": "string",
+			  "ItemOrder": 0
+			}
+		  ]
+		}
+	  ]
+	};
+	
+	$scope.questions = [];
+	$scope.items = [];
+	
+	$scope.question = '';
+	$scope.addQuestion = addQuestion;
+		
+	
+	var qOrder = 1;
+	function addQuestion(){
+		$scope.questions.push({ Question: $scope.question, QuestionOrder: qOrder++ });
+	}
 
   }]);
 

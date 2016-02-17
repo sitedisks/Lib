db2cc07830d11a938be83678c75b436165f15eeb
Peter <peter@sitedisks.com.au>
2016-02-17 21:29:24 +1100

    toastr update

--- yuyan/yuyan/js/toastr.option.js
+++ yuyan/yuyan/js/toastr.option.js
@@ -1,6 +1,6 @@
 ﻿toastr.options = {
     "progressBar": false,
-    "positionClass": "toast-top-center",
+    "positionClass": "toast-top-left",
     "showDuration": "300",
     "hideDuration": "1000",
     "timeOut": "5000",

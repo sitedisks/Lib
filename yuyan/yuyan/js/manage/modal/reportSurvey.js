a5bec380fd10bd756734b6ba5c2a6a2303dbca50
Peter Wang <peter@sitedisks.com.au>
2016-03-14 21:52:46 +1100

    answer status report popup UI

--- yuyan/yuyan/js/manage/modal/reportSurvey.js
+++ yuyan/yuyan/js/manage/modal/reportSurvey.js
@@ -6,15 +6,28 @@
             function ($scope, $uibModalInstance, survey, localStorageService, yuyanAPISvc) {
 
                 $scope.survey = survey;
-                $scope.checkedHashtb = {};
+                $scope.geoStatus = {};
+                $scope.checkedHashtb;
+                $scope.APIMini = 2;
+                $scope.APIResolved = 0;
 
                 yuyanAPISvc.surveyClientReportSvc().query({ surveyId: survey.SurveyId },
                     function (data) {
                         $scope.surveyClient = data;
 
+                        angular.forEach(data, function (client) {
+                            var address = client.City + ' ' + client.State + ', ' + client.Country;
+                            if ($scope.geoStatus[address] === undefined)
+                                $scope.geoStatus[address] = { name: address, count: 1 };
+                            else
+                                $scope.geoStatus[address] = { name: address, count: $scope.geoStatus[address].count + 1 };
+                        });
+
+                        $scope.APIResolved++;
                         yuyanAPISvc.surveyClientAnswerDicSvc().get({ surveyId: survey.SurveyId },
                            function (data) {
                                $scope.checkedHashtb = data;
+                               $scope.APIResolved++;
                            }, function () {
                                toastr.error("Error please refresh the page.");
                            });
@@ -23,8 +36,8 @@
                         toastr.error("Error please refresh the page.");
                     });
 
-               
-                              
+
+
                 $scope.ok = function () {
                     $uibModalInstance.close(surveyClient);
                 };

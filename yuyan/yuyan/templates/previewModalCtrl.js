cbf468142f9bc88f6fc3babb77252a295edeee2d
Peter <peter@sitedisks.com.au>
2016-03-20 14:58:59 +1100

    update review and share UI

--- yuyan/yuyan/templates/previewModalCtrl.js
+++ yuyan/yuyan/templates/previewModalCtrl.js
@@ -6,9 +6,14 @@
 			function ($scope, $uibModalInstance, survey) {
 
 			    $scope.survey = survey;
+			    $scope.openLink = openLink;
 
 			    $scope.shareLink = location.host + '/client/#/' + survey.URLToken;
 			    $scope.sharePageLink = location.host + '/client/#/page/' + survey.URLToken;
+
+			    function openLink(url) {
+			        window.open('http://' + url, '_blank');
+			    }
 
 			    $scope.ok = function () {
 			        $uibModalInstance.close();

6e8a36b16be04c986b49c32a2a6924d7e11ba278
Peter Wang <peter@sitedisks.com.au>
2016-10-16 08:09:01 +1100

    updates

--- UnitOfWorkRepository/UnitOfWorkRepository/Controllers/HomeController.cs
+++ UnitOfWorkRepository/UnitOfWorkRepository/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using UnitOfWorkRepository.Repository;
-
-namespace UnitOfWorkRepository.Controllers
+﻿namespace UnitOfWorkRepository.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using UnitOfWorkRepository.Models.Entity;
+    using UnitOfWorkRepository.Repository;
+
     public class HomeController : Controller
     {
         private SurveysRepository repo = new SurveysRepository();
@@ -16,7 +17,14 @@ namespace UnitOfWorkRepository.Controllers
         {
             ViewBag.Title = "Home Page";
             var list = repo.GetAll().ToList();
-            return View();
+            return View(list);
+        }
+
+        public ActionResult Details(int id = 0) {
+            tbSurvey survey = repo.Get(c => c.SurveyId == id);
+            if (survey == null)
+                return HttpNotFound();
+            return View(survey);
         }
     }
 }

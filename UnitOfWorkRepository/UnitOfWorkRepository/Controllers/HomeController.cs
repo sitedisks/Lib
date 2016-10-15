097e26750cc27c421bcaf37b3da0b2f8d7aaf74f
Peter Wang <peter@sitedisks.com.au>
2016-10-15 17:51:42 +1100

    use survey repository

--- UnitOfWorkRepository/UnitOfWorkRepository/Controllers/HomeController.cs
+++ UnitOfWorkRepository/UnitOfWorkRepository/Controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UnitOfWorkRepository.Repository;
 
 namespace UnitOfWorkRepository.Controllers
 {
     public class HomeController : Controller
     {
+        private SurveysRepository repo = new SurveysRepository();
+
         // GET: Home
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            var list = repo.GetAll().ToList();
             return View();
         }
     }

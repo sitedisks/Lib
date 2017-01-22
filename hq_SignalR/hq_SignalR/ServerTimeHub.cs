f54941e0375de79077ea48e35467238c391e1745
Peter Wang <peter@sitedisks.com.au>
2017-01-23 07:09:39 +1100

    Pushing Data: Integrating with ASP.NET SignalR Hubs

---
+++ hq_SignalR/hq_SignalR/ServerTimeHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace Henriquatre.Integration.SignalR
+{
+    public class ServerTimeHub : Hub
+    {
+        public string GetServerTime()
+        {
+            return DateTime.UtcNow.ToString();
+        }
+    }
+}

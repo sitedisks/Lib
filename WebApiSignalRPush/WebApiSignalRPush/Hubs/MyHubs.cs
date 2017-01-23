437ca01c0346722c2b2441a9a15b88f771784f07
Peter Wang <peter@sitedisks.com.au>
2017-01-24 07:02:38 +1100

    http://jaliyaudagedara.blogspot.com.au/2016/06/integrating-signalr-with-aspnet-web-api.html

--- WebApiSignalRPush/WebApiSignalRPush/Hubs/MyHubs.cs
+++ WebApiSignalRPush/WebApiSignalRPush/Hubs/MyHubs.cs
@@ -1,10 +1,8 @@
 ﻿namespace WebApiSignalRPush.Hubs
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
     using Microsoft.AspNet.SignalR;
+    using System.Threading.Tasks;
+
     public class MyHubs : Hub
     {
         public void Hello()
@@ -30,6 +28,34 @@
         public void Unsubscribe(string customerId)
         {
             Groups.Remove(Context.ConnectionId, customerId);
+        }
+
+        public override Task OnConnected()
+        {
+            // Add your own code here.
+            // For example: in a chat application, record the association between
+            // the current connection ID and user name, and mark the user as online.
+            // After the code in this method completes, the client is informed that
+            // the connection is established; for example, in a JavaScript client,
+            // the start().done callback is executed.
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            // Add your own code here.
+            // For example: in a chat application, mark the user as offline, 
+            // delete the association between the current connection id and user name.
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            // Add your own code here.
+            // For example: in a chat application, you might have marked the
+            // user as offline after a period of inactivity; in that case 
+            // mark the user as online again.
+            return base.OnReconnected();
         }
     }
 }

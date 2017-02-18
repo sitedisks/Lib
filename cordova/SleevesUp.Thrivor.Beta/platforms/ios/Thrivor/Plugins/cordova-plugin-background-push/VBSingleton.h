155b2ca924a21ba7c8dd1453f7914cb8cc0ce644
Peter Wang <peter@sitedisks.com.au>
2017-02-18 12:55:44 +1100

    Thrivor: split project to dev, uat and prod

--- cordova/SleevesUp.Thrivor.Beta/platforms/ios/Thrivor/Plugins/cordova-plugin-background-push/VBSingleton.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface VBSingleton : NSObject
-
-@property (strong,nonatomic) void (^backgroundHandler)(UIBackgroundFetchResult result);
-
-+ (VBSingleton*)sharedInstance;
-
-@end

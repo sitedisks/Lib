4847fc67ec5d60699cb358d6b2a8ca5f284752a1
Peter Wang <peter@sitedisks.com.au>
2018-01-19 16:19:16 +1100

    Add regex

--- SimpleCrawler/SimpleCrawler/Program.cs
+++ SimpleCrawler/SimpleCrawler/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleCrawler
@@ -22,6 +23,19 @@ namespace SimpleCrawler
             };
 
             cityCrawler.OnCompleted += (s, e) => {
+
+                var links = Regex.Matches(e.PageSource, @"<a[^>]+href=""*(?<href>/hotel/[^>s]+)""s*[^>]*>(?<text>(?!.*img).*?)</a>", RegexOptions.IgnoreCase);
+                foreach (Match match in links) {
+                    var city = new City
+                    {
+                        CityName = match.Groups["text"].Value,
+                        Uri = new Uri("http://hotels.ctrip.com" + match.Groups["href"].Value)
+                    };
+                    if (!cityList.Contains(city))
+                        cityList.Add(city);
+                    Console.WriteLine(city.CityName + "|" + city.Uri);
+                }
+
                 Console.WriteLine(e.PageSource);
                 Console.WriteLine("=====================================");
                 Console.WriteLine("Crawler complete!");
@@ -30,7 +44,8 @@ namespace SimpleCrawler
                 Console.WriteLine("Address: " + e.Uri.ToString());
             };
 
-            //cityCrawler.
+            cityCrawler.Start(new Uri(cityUrl), null).Wait();
+            Console.ReadKey();
         }
     }
 }

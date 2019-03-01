5f888afe19590a492da1d6742e553ad014ba4a68
Peter Wang <peter@sitedisks.com.au>
2019-03-02 05:29:13 +1100

    update the marker infowindow content

--- HealthPin/js/site.js
+++ HealthPin/js/site.js
@@ -3,7 +3,7 @@
 function loadlocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
-            function(position) {
+            function (position) {
                 //do succes handling
                 initialize(position.coords.latitude, position.coords.longitude);
             },
@@ -11,8 +11,8 @@ function loadlocation() {
                 //do error handling
             },
             {
-                maximumAge:Infinity,
-                timeout:5000
+                maximumAge: Infinity,
+                timeout: 5000
             }
         );
     } else {
@@ -82,14 +82,40 @@ function initialize(lat, lng) {
         });
 
         var content = document.createElement('div');
-        content.innerHTML =
-            '<h6>' + item.s_clinic_name + '</h6>' +
-            '<div><i class="fas fa-clinic-medical" style="font-size: 1em; color: Tomato;"></i> &nbsp;' +
-            geodata.formatted_address +
-            '</div>' +
-            '<div><i class="fas fa-phone" style="font-size: 1em; color: Tomato;"></i> &nbsp;' +
-            item.s_phone +
-            '</div>';
+
+        content.innerHTML = '<h5>' + item.s_clinic_name + '</h5>'
+            + '<hr class="marker-divider">'
+            + '<table>'
+            + '<tr>'
+            + '<td><i class="fas fa-clinic-medical" style="font-size: 1em; color: #78C2AD;"></i></td>'
+            + '<td>' + item.s_address + '</td>'
+            + '</tr>'
+            + '<tr>'
+            + '<td><i class="fas fa-phone" style="font-size: 1em; color: #78C2AD;"></i></td>'
+            + '<td>' + item.s_phone + '</td>'
+            + '</tr>'
+            + '<tr>'
+            + '<td style="width: 20%; padding-top: 10px"><i class="fas fa-hospital-symbol" style="font-size: 1em; color: #78C2AD;"></i></td>'
+            + '<td style="width: 80%; padding-top: 10px">' + item.s_specialties + '</td>'
+            + '</tr>'
+            + '</table>'
+            + '<hr class="marker-divider">'
+            + '<div class="star-rating py-1" style="color: #FFCE67">'
+            + '<span class="fas fa-star"></span>'
+            + '<span class="fas fa-star"></span>'
+            + '<span class="fas fa-star"></span>'
+            + '<span class="fas fa-star"></span>'
+            + '<span class="far fa-star"></span>'
+            + '</div>'
+            + '<small class="text-muted pr-3">'
+            + '<i class="far fa-share-square"></i> 34'
+            + '</small>'
+            + '<small class="text-muted pr-3">'
+            + '<i class="far fa-comment-dots"></i> 87'
+            + '</small>'
+            + '<small class="text-muted pr-3">'
+            + '<i class="far fa-thumbs-up"></i> 28'
+            + '</small>'
 
         var html_card = $('div#marker_card').html().replace('__placeholder__', content.innerHTML);
 

baecd48cff1498889f35ea216ff3d28a88aa3564
Peter Wang <peter@sitedisks.com.au>
2019-03-07 07:12:15 +1100

    bit improve

--- cordova/OnsenUI/www/js/app.js
+++ cordova/OnsenUI/www/js/app.js
@@ -14,10 +14,20 @@ $(function () {
         pageNav(url);
     })
 
-    function pageNav(url){
-        if(window.location.pathname != url)
+    function pageNav(url) {
+        if (window.location.pathname != url)
             window.location.replace(url);
     }
+
+    $('span.idea').on('click',function(){
+        var modal = document.querySelector('ons-modal');
+        modal.show();
+        setTimeout(function () {
+            modal.hide();
+        }, 2000);
+    })
+
+    
 
     $('ons-button').on('click', function (e) {
         ons.notification.alert('Button is tapped!');

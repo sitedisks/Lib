f4b19e8cb186752356db3c078d3811aae4ac1090
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-03-04 09:33:13 +1100

    https://www.jianshu.com/p/66d829aeda4c

--- Jasmine/Jasmine/test2.js
+++ Jasmine/Jasmine/test2.js
@@ -1,4 +1,4 @@
-describe('A spec using beforeEach and afterEach', function () {
+describe('A spec using beforeEach and afterEach!', function () {
     var foo = 0;
 
     beforeEach(() => {
@@ -10,13 +10,13 @@ describe('A spec using beforeEach and afterEach', function () {
     });
 
 
-    it('is just a function, so it can contain any code', () => {
-        expect(foo).toEqual(1);
-       
+    it('this will not pass', () => {
+        expect(foo).toEqual(2);
+
     });
 
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+    it("says hello", function () {
+        expect(helloWorld()).toEqual("Hello world!!");
     });
 
     it('can have more than one expectation', () => {
@@ -25,30 +25,30 @@ describe('A spec using beforeEach and afterEach', function () {
     });
 });
 
-describe("A spy", function() {
+describe("A spy", function () {
     var foo, bar = null;
-  
-    beforeEach(function() {
-      foo = { setBar:function(value){bar = value;} };
-      spyOn(foo, 'setBar');   //关键，设定要监听的对象的方法
-      foo.setBar(123);  
-      foo.setBar(456, 'another param');
+
+    beforeEach(function () {
+        foo = { setBar: function (value) { bar = value; } };
+        spyOn(foo, 'setBar');   //关键，设定要监听的对象的方法
+        foo.setBar(123);
+        foo.setBar(456, 'another param');
     });
-  
-    it("tracks that the spy was called", function() {
-      expect(foo.setBar).toHaveBeenCalled();
+
+    it("tracks that the spy was called", function () {
+        expect(foo.setBar).toHaveBeenCalled();
     });
-  
-    it("tracks that the spy was called x times", function() {
-      expect(foo.setBar).toHaveBeenCalledTimes(2);
+
+    it("tracks that the spy was called x times", function () {
+        expect(foo.setBar).toHaveBeenCalledTimes(2);
     });
-  
-    it("tracks all the arguments of its calls", function() {
-      expect(foo.setBar).toHaveBeenCalledWith(123);
-      expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+
+    it("tracks all the arguments of its calls", function () {
+        expect(foo.setBar).toHaveBeenCalledWith(123);
+        expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
     });
-  
-    it("stops all execution on a function", function() {
-      expect(bar).toBeNull();
+
+    it("stops all execution on a function", function () {
+        expect(bar).toBeNull();
     });
-  });
+});

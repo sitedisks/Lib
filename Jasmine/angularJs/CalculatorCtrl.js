1f429b8061c73a5e1242227e7164b6fc91c65254
Peter@Desktop6700 <peter@sitedisks.com.au>
2020-03-12 12:23:23 +1100

    hao

--- Jasmine/angularJs/CalculatorCtrl.js
+++ Jasmine/angularJs/CalculatorCtrl.js
@@ -1,15 +1,21 @@
-angular
-    .module('app', [])
-    .controller('calculatorController', function ($scope) {
-        // var vm = this;
-        $scope.message = 'I am a simple calculator';
-        // vm.result = 0;
-        // vm.firstNumber = 0;
-        // vm.secondNumber = 0;
-        // vm.addNumber = addNumber;
-    
-        // function addNumber() {
-        //     vm.result = vm.firstNumber + vm.secondNumber;
-        // }
-    
-    });
+(function () {
+
+    'use strict';
+
+    angular
+        .module('app', [])
+        .controller('calculatorController', function ($scope) {
+            // var vm = this;
+            $scope.message = 'I am a simple calculator';
+            // vm.result = 0;
+            // vm.firstNumber = 0;
+            // vm.secondNumber = 0;
+            // vm.addNumber = addNumber;
+
+            // function addNumber() {
+            //     vm.result = vm.firstNumber + vm.secondNumber;
+            // }
+
+        });
+})();
+

099c9abe2accf5c3b01d30b153ae8d5a5cad7a27
Peter@ROG <peter@sitedisks.com.au>
2020-03-30 12:12:20 +1100

    tobe - ionic

--- tobe/src/app/app.component.ts
+++ tobe/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoronaService } from './service/corona.service';
+import { Corona } from './model/corona.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,48 @@ import { CoronaService } from './service/corona.service';
 export class AppComponent implements OnInit {
   title = 'tobe';
 
-  cases:any = [];
+  time = new Date();
+  timer;
+  isLoading: boolean = false;
+  cases: any = [];
+  status: Corona = {cases: 0, deaths: 0, recovered: 0, updated: 0, active: 0};
+
   constructor(private data: CoronaService) { }
 
-  ngOnInit() { 
+  reload() {
+    this.isLoading = true;
+    console.log('data reloaded');
     this.data.getDataByCountries().subscribe(
-      result=>{
-        console.log(result);
+      result => {
+        this.isLoading = false;
         this.cases = result;
       }
     );
+  };
+
+  all() {
+    this.data.getAllStatus().subscribe(
+      result => {
+        console.log(result);
+        this.status = result;
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+    this.all();
+    this.reload();
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.timer);
+  }
+
+  handleClick(){
+    this.all();
+    this.reload();
   }
 }
